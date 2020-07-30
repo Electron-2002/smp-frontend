39962674ed719a9ad76c4d385cebe45450e3aa1c
Satej Bidvai <satej.bidvai@gmail.com>
2020-07-30 16:59:33 +0530

    Options for Hospital/Patient-Register added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
-import "./App.css";
-import Records from "./components/Records";
+import React from 'react';
+import './App.css';
+import Records from './components/Records';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import RegisterPatient from './components/RegisterPatient';
 
 function App() {
 	return (
-		<div className="container">
-			<div className="card shadow-lg">
-				<div className="card-header text-center">Symptom Checker</div>
-				<div id="step-container" className="card-block overflow-auto">
-					<Records />
-				</div>
-				<div className="card-footer">
-					<button
-						id="next-step"
-						className="btn btn-primary float-right"
-					>
-						Next <i className="fa fa-chevron-right"></i>
-					</button>
+		<div className='container mt-5'>
+			<div className='card shadow-lg'>
+				<div className='card-header text-center'>Symptom Checker</div>
+				<div id='step-container' className='card-block overflow-auto'>
+					<Router>
+						<Switch>
+							<Route path='/hospital' exact component={Records} />
+							<Route
+								path='/register-patient'
+								exact
+								component={RegisterPatient}
+							/>
+							<Route path='/' exact component={Home} />
+						</Switch>
+					</Router>
 				</div>
 			</div>
 		</div>
---
+++ src/components/RegisterPatient.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterPatient = () => {
+	return <div></div>;
+};
+
+export default RegisterPatient;
